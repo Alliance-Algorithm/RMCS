b1bf33a3a923e6ea3d2ea4fb002c664a8839a83c
世羽 <sy2330@qq.com>
2023-11-24 21:38:36 +0800

    Update hardware interface

--- rmcs_ws/src/rmcs_hardware_interface/src/rmcs_hardware_interface.cpp
+++ rmcs_ws/src/rmcs_hardware_interface/src/rmcs_hardware_interface.cpp
@@ -28,7 +28,7 @@ hardware_interface::CallbackReturn
     }
 
     // TODO(anyone): read parameters and initialize the hardware
-    // serialport_ = info_.hardware_parameters["serialport"];
+    serialport_ = info_.hardware_parameters["serialport"];
 
     hw_states_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
     hw_commands_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
@@ -39,7 +39,7 @@ hardware_interface::CallbackReturn
 hardware_interface::CallbackReturn
     RMCS_System::on_configure(const rclcpp_lifecycle::State& /*previous_state*/) {
     // TODO(anyone): prepare the robot to be ready for read calls and write calls of some interfaces
-    // serial_.open(serialport_);
+    serial_.open(serialport_);
 
     return CallbackReturn::SUCCESS;
 }
