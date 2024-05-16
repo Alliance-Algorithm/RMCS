7c427b35f85ddfbffa3110c17bd3bc7148c61a1f
7emotions <paradise_c@qq.com>
2024-05-16 19:43:11 +0000

    # Feature:  1- Receive robotic color, id and auto_rune from uart.  2- Define robotic color descriptor. # Refactor:  1- Rewrite update_quaternion to update additional data.  2- Adopt static const variables to describe package header.

---
+++ rmcs_ws/src/rmcs_description/include/rmcs_description/robotic_description.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cstdint>
+namespace rmcs_description {
+enum class RoboticColor : uint8_t {
+    Blue    = 0, // Blue
+    Red     = 1, // Red
+    Neutral = 2, // Neutral Part
+};
+}
