3553f502a57353327ba74e528d7a07980cb8a283
7emotions <paradise_c@qq.com>
2024-05-29 14:28:11 +0000

    :sparkles: Introduce config and launch for infantry

--- .script/sync-remote.sh
+++ .script/sync-remote.sh
@@ -3,4 +3,4 @@
 SRC_DIR=rmcs_ws/install
 DST_DIR=ssh://remote//rmcs_install
 
-unison -auto -batch -repeat watch -times ${SRC_DIR} ${DST_DIR} -force ${SRC_DIR}
+unison -ignorearchives -auto -batch -repeat watch -times ${SRC_DIR} ${DST_DIR} -force ${SRC_DIR}
