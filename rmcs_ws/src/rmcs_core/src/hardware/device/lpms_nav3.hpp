7eb9a82e7b711cc97682b6ce073d8543276ded2d
creeper5820 <cqq1960180796@163.com>
2025-02-22 02:47:52 +0800

    Prepare add extra imu but lpms nav3 is broken

---
+++ rmcs_ws/src/rmcs_core/src/hardware/device/lpms_nav3.hpp
@@ -0,0 +1,10 @@
+
+#pragma once
+
+#include <librmcs/device/lpms_nav3.hpp>
+
+namespace rmcs_core::hardware::device {
+
+using LpmsNav3 = librmcs::device::LpmsNav3;
+
+} // namespace rmcs_core::hardware::device
