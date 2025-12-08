4f1b3ffe497a40a70eef5972aeb9bd50512bdc7b
floatpigeon <floatpigeon@proton.me>
2025-12-08 19:50:43 +0800

    Pick basic controllers from dart branch

---
+++ rmcs_ws/src/rmcs_core/src/controller/dartlauncher/launch_controller.cpp
@@ -0,0 +1,206 @@
+#include "controller/pid/matrix_pid_calculator.hpp"
+#include <algorithm>
+#include <cstdlib>
+#include <eigen3/Eigen/Dense>
+#include <rclcpp/logger.hpp>
+#include <rclcpp/logging.hpp>
+#include <rclcpp/node.hpp>
+#include <rmcs_executor/component.hpp>
+#include <rmcs_msgs/dart_launch_stage.hpp>
+#include <rmcs_msgs/switch.hpp>
+#include <sys/types.h>
+
+/*
+键位：
+双下：全部停止
+双中：初始状态
+    此时{
+        右拨杆下拨再回中：切换上膛和退膛
+        处于上膛状态时右拨杆打到上：发射
+    }
+左拨杆上：设置模式
+    此时{
+        右拨杆在中：调整角度，左右摇杆分别控制yaw和pitch以防误触
+        右拨杆在下：调整拉力，在yaml中设置力闭环模式或者手动控制模式
+    }
+*/
+
+namespace rmcs_core::controller::dart {
+class DartLaunchController
+    : public rmcs_executor::Component
+    , public rclcpp::Node {
+public:
+    DartLaunchController()
+        : Node{
+              get_component_name(),
+              rclcpp::NodeOptions{}.automatically_declare_parameters_from_overrides(true)}
+        , logger_(get_logger())
+        , drive_belt_pid_calculator_(
+              get_parameter("b_kp").as_double(), get_parameter("b_ki").as_double(),
+              get_parameter("b_kd").as_double()) {
+
+        dirve_belt_working_velocity_ = get_parameter("belt_velocity").as_double();
+        sync_coefficient_ = get_parameter("sync_coefficient").as_double();
+        max_control_torque_ = get_parameter("max_control_torque").as_double();
+
+        launch_trigger_angle_ = get_parameter("trigger_free_angle").as_int();
+        launch_lock_angle_ = get_parameter("trigger_lock_angle").as_int();
+
+        register_input("/remote/joystick/right", joystick_right_);
+        register_input("/remote/joystick/left", joystick_left_);
+        register_input("/remote/switch/right", switch_right_);
+        register_input("/remote/switch/left", switch_left_);
+
+        register_input("/dart/drive_belt/left/velocity", left_drive_belt_velocity_);
+        register_input("/dart/drive_belt/right/velocity", right_drive_belt_velocity_);
+
+        register_output("/dart/drive_belt/left/control_torque", left_drive_belt_control_torque_, 0);
+        register_output(
+            "/dart/drive_belt/right/control_torque", right_drive_belt_control_torque_, 0);
+
+        register_output("/dart/trigger_servo/control_angle", trigger_control_angle);
+    }
+
+    void update() override {
+        using namespace rmcs_msgs;
+
+        if ((*switch_left_ == Switch::DOWN || *switch_left_ == Switch::UNKNOWN)
+            && (*switch_right_ == Switch::DOWN || *switch_right_ == Switch::UNKNOWN)) {
+            *launch_stage_ = DartLaunchStages::DISABLE;
+            reset_all_controls();
+
+        } else if (*switch_left_ == Switch::MIDDLE) {
+
+            if (last_launch_stage_ == DartLaunchStages::DISABLE) {
+                *launch_stage_ = DartLaunchStages::RESETTING;
+            }
+
+            if (last_switch_right_ == Switch::MIDDLE && *switch_right_ == Switch::DOWN) {
+                if (last_launch_stage_ == DartLaunchStages::INIT) {
+                    *launch_stage_ = DartLaunchStages::LOADING;
+                } else if (last_launch_stage_ == DartLaunchStages::READY) {
+                    *launch_stage_ = DartLaunchStages::CANCEL;
+                }
+            } else if (last_switch_right_ == Switch::MIDDLE && *switch_right_ == Switch::UP) {
+                if (last_launch_stage_ == DartLaunchStages::READY) {
+                    *launch_stage_ = DartLaunchStages::INIT;
+                    trigger_lock_flag_ = false;
+                } else {
+                    RCLCPP_INFO(logger_, "Dart has't been loaded !");
+                }
+            }
+
+            if (blocking_detection()) {
+                if (last_launch_stage_ == DartLaunchStages::LOADING) {
+                    *launch_stage_ = DartLaunchStages::RESETTING;
+                    trigger_lock_flag_ = true;
+                    dirve_belt_block_count_ = 0;
+
+                } else if (last_launch_stage_ == DartLaunchStages::CANCEL) {
+                    *launch_stage_ = DartLaunchStages::RESETTING;
+                    trigger_lock_flag_ = false;
+                    dirve_belt_block_count_ = 0;
+
+                } else if (last_launch_stage_ == DartLaunchStages::RESETTING) {
+                    *launch_stage_ =
+                        trigger_lock_flag_ ? DartLaunchStages::READY : DartLaunchStages::INIT;
+                    dirve_belt_block_count_ = 0;
+                }
+            }
+            double control_velocity = 0;
+
+            if (*launch_stage_ == rmcs_msgs::DartLaunchStages::RESETTING) {
+                control_velocity = -dirve_belt_working_velocity_;
+            } else if (
+                *launch_stage_ == rmcs_msgs::DartLaunchStages::LOADING
+                || *launch_stage_ == rmcs_msgs::DartLaunchStages::CANCEL) {
+                control_velocity = dirve_belt_working_velocity_;
+            } else {
+                control_velocity = 0;
+            }
+            drive_belt_sync_control(control_velocity);
+            RCLCPP_INFO(logger_, "%lf", control_velocity);
+        }
+
+        *trigger_control_angle = trigger_lock_flag_ ? launch_lock_angle_ : launch_trigger_angle_;
+
+        last_switch_left_ = *switch_left_;
+        last_switch_right_ = *switch_right_;
+        last_launch_stage_ = *launch_stage_;
+    }
+
+private:
+    void reset_all_controls() {
+        *launch_stage_ = rmcs_msgs::DartLaunchStages::DISABLE;
+        *left_drive_belt_control_torque_ = 0;
+        *right_drive_belt_control_torque_ = 0;
+    }
+
+    void drive_belt_sync_control(double set_velocity) {
+        if (set_velocity == 0) {
+            *left_drive_belt_control_torque_ = 0;
+            *right_drive_belt_control_torque_ = 0;
+            return;
+        }
+
+        Eigen::Vector2d setpoint_error{
+            set_velocity - *left_drive_belt_velocity_, set_velocity - *right_drive_belt_velocity_};
+
+        Eigen::Vector2d relative_velocity{
+            *left_drive_belt_velocity_ - *right_drive_belt_velocity_,
+            *right_drive_belt_velocity_ - *left_drive_belt_velocity_};
+
+        Eigen::Vector2d control_torques = setpoint_error - sync_coefficient_ * relative_velocity;
+
+        *left_drive_belt_control_torque_ =
+            std::clamp(control_torques[0], -max_control_torque_, max_control_torque_);
+        *right_drive_belt_control_torque_ =
+            std::clamp(control_torques[1], -max_control_torque_, max_control_torque_);
+    }
+
+    bool blocking_detection() {
+        if ((abs(*left_drive_belt_velocity_) < 0.5 && abs(*left_drive_belt_control_torque_) > 0.5)
+            || (abs(*right_drive_belt_velocity_) < 0.5
+                && abs(*right_drive_belt_control_torque_) > 0.5)) {
+            dirve_belt_block_count_++;
+        }
+
+        return dirve_belt_block_count_ > 1000 ? true : false;
+    }
+
+    int dirve_belt_block_count_ = 0;
+    double dirve_belt_working_velocity_;
+
+    rclcpp::Logger logger_;
+
+    InputInterface<Eigen::Vector2d> joystick_right_;
+    InputInterface<Eigen::Vector2d> joystick_left_;
+    InputInterface<rmcs_msgs::Switch> switch_right_;
+    InputInterface<rmcs_msgs::Switch> switch_left_;
+    rmcs_msgs::Switch last_switch_right_;
+    rmcs_msgs::Switch last_switch_left_;
+
+    OutputInterface<double> left_drive_belt_control_torque_;
+    OutputInterface<double> right_drive_belt_control_torque_;
+    InputInterface<double> left_drive_belt_velocity_;
+    InputInterface<double> right_drive_belt_velocity_;
+
+    double max_control_torque_;
+
+    OutputInterface<rmcs_msgs::DartLaunchStages> launch_stage_;
+    rmcs_msgs::DartLaunchStages last_launch_stage_ = rmcs_msgs::DartLaunchStages::DISABLE;
+
+    uint16_t launch_lock_angle_;
+    uint16_t launch_trigger_angle_;
+    bool trigger_lock_flag_ = false;
+    OutputInterface<uint16_t> trigger_control_angle;
+
+    pid::MatrixPidCalculator<2> drive_belt_pid_calculator_;
+    double sync_coefficient_;
+};
+
+} // namespace rmcs_core::controller::dart
+
+#include <pluginlib/class_list_macros.hpp>
+
+PLUGINLIB_EXPORT_CLASS(rmcs_core::controller::dart::DartLaunchController, rmcs_executor::Component)
---
+++ rmcs_ws/src/rmcs_core/src/hardware/catapult_dart.cpp
@@ -0,0 +1,266 @@
+#include "hardware/device/dji_motor.hpp"
+#include "hardware/device/dr16.hpp"
+#include "hardware/device/force_sensor.hpp"
+#include "hardware/device/trigger_servo.hpp"
+#include "librmcs/client/cboard.hpp"
+#include <cstddef>
+#include <rclcpp/logger.hpp>
+#include <rclcpp/logging.hpp>
+#include <rclcpp/node.hpp>
+#include <rmcs_executor/component.hpp>
+#include <rmcs_msgs/serial_interface.hpp>
+#include <std_msgs/msg/int32.hpp>
+
+namespace rmcs_core::hardware {
+class CatapultDart
+    : public rmcs_executor::Component
+    , public rclcpp::Node
+    , private librmcs::client::CBoard {
+public:
+    CatapultDart()
+        : Node{
+              get_component_name(),
+              rclcpp::NodeOptions{}.automatically_declare_parameters_from_overrides(true)}
+        , librmcs::client::CBoard{static_cast<int>(get_parameter("usb_pid").as_int())}
+        , logger_(get_logger())
+        , dart_command_(
+              create_partner_component<DartCommand>(get_component_name() + "_command", *this))
+        , dr16_(*this)
+        , pitch_motor_(
+              *this, *dart_command_, "/dart/pitch_motor",
+              device::DjiMotor::Config{device::DjiMotor::Type::M3508}.set_reduction_ratio(19.))
+        , yaw_motor_(
+              *this, *dart_command_, "/dart/yaw_motor",
+              device::DjiMotor::Config{device::DjiMotor::Type::M3508}.set_reduction_ratio(19.))
+        , force_control_motor_(
+              *this, *dart_command_, "/dart/force_screw_motor",
+              device::DjiMotor::Config{device::DjiMotor::Type::M3508}.set_reduction_ratio(19.))
+        , drive_belt_motor_(
+              {*this, *dart_command_, "/dart/drive_belt/left",
+               device::DjiMotor::Config{device::DjiMotor::Type::M3508}.set_reduction_ratio(19.)},
+              {*this, *dart_command_, "/dart/drive_belt/right",
+               device::DjiMotor::Config{device::DjiMotor::Type::M3508}
+                   .set_reduction_ratio(19.)
+                   .set_reversed()})
+        , force_sensor_(*this)
+        , trigger_servo_(*dart_command_, "/dart/trigger_servo")
+        , transmit_buffer_(*this, 32)
+        , event_thread_([this]() { handle_events(); }) {
+
+        trigger_calibrate_subscription_ = create_subscription<std_msgs::msg::Int32>(
+            "/trigger/calibrate", rclcpp::QoS{0}, [this](std_msgs::msg::Int32::UniquePtr&& msg) {
+                trigger_servo_calibrate_subscription_callback(std::move(msg));
+            });
+
+        register_output("/referee/serial", referee_serial_);
+        referee_serial_->read = [this](std::byte* buffer, size_t size) {
+            return referee_ring_buffer_receive_.pop_front_multi(
+                [&buffer](std::byte byte) { *buffer++ = byte; }, size);
+        };
+        referee_serial_->write = [this](const std::byte* buffer, size_t size) {
+            transmit_buffer_.add_uart1_transmission(buffer, size);
+            return size;
+        };
+    }
+
+    ~CatapultDart() override {
+        stop_handling_events();
+        event_thread_.join();
+    }
+
+    void update() override {
+        dr16_.update_status();
+        pitch_motor_.update_status();
+        yaw_motor_.update_status();
+        drive_belt_motor_[0].update_status();
+        drive_belt_motor_[1].update_status();
+        force_control_motor_.update_status();
+        force_sensor_.update_status();
+    }
+
+    void command_update() {
+        uint16_t can_commands[4];
+
+        if (force_sensor_pub_time_count_++ > 100) {
+            force_sensor_pub_time_count_ = 0;
+
+            transmit_buffer_.add_can1_transmission(
+                force_sensor_.get_can_id(device::ForceSensor::Mode::MEASUREMENT, 1),
+                force_sensor_.generate_read_weight_command());
+        }
+
+        can_commands[0] = pitch_motor_.generate_command();
+        can_commands[1] = yaw_motor_.generate_command();
+        can_commands[2] = force_control_motor_.generate_command();
+        can_commands[3] = 0;
+        transmit_buffer_.add_can2_transmission(0x200, std::bit_cast<uint64_t>(can_commands));
+
+        can_commands[0] = drive_belt_motor_[0].generate_command();
+        can_commands[1] = drive_belt_motor_[1].generate_command();
+        can_commands[2] = 0;
+        can_commands[3] = 0;
+        transmit_buffer_.add_can2_transmission(0x1FF, std::bit_cast<uint64_t>(can_commands));
+
+        if (!trigger_servo_.calibrate_mode()) {
+            size_t uart_data_length;
+            std::unique_ptr<std::byte[]> command_buffer =
+                trigger_servo_.generate_runtime_command(uart_data_length);
+            const auto trigger_servo_uart_data_ptr = command_buffer.get();
+            transmit_buffer_.add_uart2_transmission(trigger_servo_uart_data_ptr, uart_data_length);
+        }
+
+        transmit_buffer_.trigger_transmission();
+    }
+
+protected:
+    void can1_receive_callback(
+        uint32_t can_id, uint64_t can_data, bool is_extended_can_id, bool is_remote_transmission,
+        uint8_t can_data_length) override {
+
+        if (is_extended_can_id || is_remote_transmission || can_data_length < 8) [[unlikely]]
+            return;
+
+        if (can_id == 0x302) {
+            force_sensor_.store_status(can_data);
+        }
+    }
+
+    void can2_receive_callback(
+        uint32_t can_id, uint64_t can_data, bool is_extended_can_id, bool is_remote_transmission,
+        uint8_t can_data_length) override {
+        if (is_extended_can_id || is_remote_transmission || can_data_length < 8) [[unlikely]]
+            return;
+
+        if (can_id == 0x201) {
+            pitch_motor_.store_status(can_data);
+        } else if (can_id == 0x202) {
+            yaw_motor_.store_status(can_data);
+        } else if (can_id == 0x203) {
+            force_control_motor_.store_status(can_data);
+        } else if (can_id == 0x205) {
+            drive_belt_motor_[0].store_status(can_data);
+        } else if (can_id == 0x206) {
+            drive_belt_motor_[1].store_status(can_data);
+        }
+    }
+
+    void uart1_receive_callback(const std::byte* uart_data, uint8_t uart_data_length) override {
+        referee_ring_buffer_receive_.emplace_back_multi(
+            [&uart_data](std::byte* storage) { *storage = *uart_data++; }, uart_data_length);
+    }
+
+    void uart2_receive_callback(const std::byte* data, uint8_t length) override {
+        bool success = trigger_servo_.calibrate_current_angle(logger_, data, length);
+        if (!success) {
+            RCLCPP_INFO(logger_, "calibrate: uart2 data store failed");
+        }
+
+        // std::string hex_string = bytes_to_hex_string(data, length);
+        // RCLCPP_INFO(this->get_logger(), "UART2(length: %hhu): [ %s ]", length,
+        // hex_string.c_str());
+    }
+
+    void dbus_receive_callback(const std::byte* uart_data, uint8_t uart_data_length) override {
+        dr16_.store_status(uart_data, uart_data_length);
+    }
+
+private:
+    void trigger_servo_calibrate_subscription_callback(std_msgs::msg::Int32::UniquePtr msg) {
+        /*
+        标定命令格式：
+        ros2 topic pub --rate 2 --times 5 /trigger/calibrate std_msgs/msg/Int32 "{'data':0}"
+        替换data值就行
+        */
+        trigger_servo_.set_calibrate_mode(msg->data);
+
+        std::unique_ptr<std::byte[]> command_buffer;
+        size_t command_length = 0;
+        if (msg->data == 0) {
+            command_buffer = trigger_servo_.generate_calibrate_command(
+                device::TriggerServo::CalibrateOperation::SWITCH_TO_SERVO_MODE, command_length);
+        } else if (msg->data == 1) {
+            command_buffer = trigger_servo_.generate_calibrate_command(
+                device::TriggerServo::CalibrateOperation::SWITCH_TO_MOTOR_MODE, command_length);
+        } else if (msg->data == 2) {
+            command_buffer = trigger_servo_.generate_calibrate_command(
+                device::TriggerServo::CalibrateOperation::MOTOR_FORWARD_MODE, command_length);
+        } else if (msg->data == 3) {
+            command_buffer = trigger_servo_.generate_calibrate_command(
+                device::TriggerServo::CalibrateOperation::MOTOR_REVERSE_MODE, command_length);
+        } else if (msg->data == 4) {
+            command_buffer = trigger_servo_.generate_calibrate_command(
+                device::TriggerServo::CalibrateOperation::MOTOR_RUNTIME_CONTROL, command_length);
+        } else if (msg->data == 5) {
+            command_buffer = trigger_servo_.generate_calibrate_command(
+                device::TriggerServo::CalibrateOperation::MOTOR_DISABLE_CONTROL, command_length);
+        } else if (msg->data == 6) {
+            command_buffer = trigger_servo_.generate_calibrate_command(
+                device::TriggerServo::CalibrateOperation::READ_CURRENT_ANGLE, command_length);
+        }
+
+        const auto trigger_servo_uart_data_ptr = command_buffer.get();
+        transmit_buffer_.add_uart2_transmission(trigger_servo_uart_data_ptr, command_length);
+
+        std::string hex_string = bytes_to_hex_string(command_buffer.get(), command_length);
+        RCLCPP_INFO(
+            this->get_logger(), "UART2 Pub: (length=%zu)[ %s ]", command_length,
+            hex_string.c_str());
+    }
+
+    static std::string bytes_to_hex_string(const std::byte* data, size_t size) {
+        if (!data || size == 0) {
+            return "[]";
+        }
+
+        std::stringstream ss;
+        ss << std::hex << std::uppercase << std::setfill('0');
+
+        for (size_t i = 0; i < size; ++i) {
+            ss << std::setw(2) << static_cast<int>(data[i]) << " ";
+        }
+
+        std::string result = ss.str();
+        if (!result.empty() && result.back() == ' ') {
+            result.pop_back();
+        }
+        return result;
+    } // DEBUG TOOL
+
+    rclcpp::Logger logger_;
+
+    class DartCommand : public rmcs_executor::Component {
+    public:
+        explicit DartCommand(CatapultDart& robot)
+            : dart_(robot) {}
+
+        void update() override { dart_.command_update(); }
+
+        CatapultDart& dart_;
+    };
+    std::shared_ptr<DartCommand> dart_command_;
+
+    device::Dr16 dr16_;
+
+    device::DjiMotor pitch_motor_;
+    device::DjiMotor yaw_motor_;
+    device::DjiMotor force_control_motor_;
+    device::DjiMotor drive_belt_motor_[2];
+
+    device::ForceSensor force_sensor_;
+    int force_sensor_pub_time_count_ = 0; // ForceSensor仅支持10Hz
+
+    device::TriggerServo trigger_servo_;
+
+    rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr trigger_calibrate_subscription_;
+
+    librmcs::utility::RingBuffer<std::byte> referee_ring_buffer_receive_{256};
+    OutputInterface<rmcs_msgs::SerialInterface> referee_serial_;
+
+    librmcs::client::CBoard::TransmitBuffer transmit_buffer_;
+    std::thread event_thread_;
+};
+} // namespace rmcs_core::hardware
+
+#include <pluginlib/class_list_macros.hpp>
+
+PLUGINLIB_EXPORT_CLASS(rmcs_core::hardware::CatapultDart, rmcs_executor::Component)
