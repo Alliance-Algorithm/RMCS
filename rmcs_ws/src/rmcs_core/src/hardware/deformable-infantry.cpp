121210bbd867e158da88f1a6fe4143bd32791dd9
ZGZ713912 <zgz713912@gmail.com>
2026-01-12 20:25:47 +0800

    Change YAML of smc

--- rmcs_ws/src/rmcs_core/src/hardware/deformable-infantry.cpp
+++ rmcs_ws/src/rmcs_core/src/hardware/deformable-infantry.cpp
@@ -423,7 +423,7 @@ private:
                 can_commands[0] = chassis_steer_motors_[0].generate_command();
                 can_commands[1] = 0;
                 can_commands[2] = 0;
-                can_commands[3] = 0;
+                can_commands[3] = /*static_cast<uint16_t>(supercap_.generate_command())*/0;
                 transmit_buffer_.add_can2_transmission(0x1FE, std::bit_cast<uint64_t>(can_commands));
 
                 can_commands[0] = chassis_wheel_motors_[1].generate_command();
@@ -437,10 +437,10 @@ private:
                 can_commands[2] = gimbal_bullet_feeder_.generate_command();
                 can_commands[3] = 0;
                 transmit_buffer_.add_can2_transmission(0x200, std::bit_cast<uint64_t>(can_commands));
-;
+
+            transmit_buffer_.trigger_transmission();
         }
 
-            transmit_buffer_.trigger_transmission()
         void can1_receive_callback(
             uint32_t can_id, uint64_t can_data, bool is_extended_can_id,
             bool is_remote_transmission, uint8_t can_data_length) override {
@@ -451,7 +451,7 @@ private:
             else if (can_id == 0x202) chassis_joint_motors_[1].store_status(can_data);
             else if (can_id == 0x203) gimbal_bullet_feeder_.store_status(can_data);
             else if (can_id == 0x208) chassis_steer_motors_[1].store_status(can_data);
-            else if (can_id == 0x300) supercap_.store_status(can_data);
+            // else if (can_id == 0x300) supercap_.store_status(can_data);
         }
 
         void can2_receive_callback(
