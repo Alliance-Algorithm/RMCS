2b2b753406ac8aea555bf0f2e0309237b4585f20
floatpigeon <floatpigeon@proton.me>
2026-03-18 22:06:30 +0800

    Update dart integration and detach rmcs_dart_guidance

--- rmcs_ws/src/rmcs_core/src/controller/dart/dart_setting.cpp
+++ rmcs_ws/src/rmcs_core/src/controller/dart/dart_setting.cpp
@@ -26,12 +26,15 @@ public:
               get_parameter("pitch_kd").as_double()} {
 
         register_input("/pitch/control/velocity", yaw_pitch_vel_setpoint_);
-        register_input("/dart/yaw_motor/velocity",   yaw_velocity_);
+        register_input("/dart/yaw_motor/velocity", yaw_velocity_);
         register_input("/dart/pitch_motor/velocity", pitch_velocity_);
         register_input("/force/control/velocity", force_vel_setpoint_);
 
-        register_output("/dart/yaw_motor/control_torque",   yaw_torque_,   0.0);
+        register_output("/dart/yaw_motor/control_torque", yaw_torque_, 0.0);
         register_output("/dart/pitch_motor/control_torque", pitch_torque_, 0.0);
+
+        register_input("/force_sensor/channel_1/weight", weight_ch1_);
+        register_input("/force_sensor/channel_2/weight", weight_ch2_);
 
         try {
             log_enable_ = get_parameter("log_enable").as_bool();
@@ -42,12 +45,12 @@ public:
 
     void update() override {
         const Eigen::Vector2d& yaw_pitch_velocity = *yaw_pitch_vel_setpoint_;
-        *yaw_torque_   = yaw_pid_.update(yaw_pitch_velocity[0] - *yaw_velocity_);
+        *yaw_torque_ = yaw_pid_.update(yaw_pitch_velocity[0] - *yaw_velocity_);
         *pitch_torque_ = pitch_pid_.update(yaw_pitch_velocity[1] - *pitch_velocity_);
 
         if (log_enable_) {
             if (log_counter_++ >= 1000) {
-                RCLCPP_INFO(get_logger(), "Manual force control velocity: %.3f", *force_vel_setpoint_);
+                RCLCPP_INFO(get_logger(), "ch1 : %d | ch2 : %d", *weight_ch1_, *weight_ch2_);
                 log_counter_ = 0;
             }
         }
@@ -58,12 +61,15 @@ private:
     pid::PidCalculator pitch_pid_;
 
     InputInterface<Eigen::Vector2d> yaw_pitch_vel_setpoint_;
-    InputInterface<double>          yaw_velocity_;
-    InputInterface<double>          pitch_velocity_;
-    InputInterface<double>          force_vel_setpoint_;
+    InputInterface<double> yaw_velocity_;
+    InputInterface<double> pitch_velocity_;
+    InputInterface<double> force_vel_setpoint_;
 
     OutputInterface<double> yaw_torque_;
     OutputInterface<double> pitch_torque_;
+
+    InputInterface<int> weight_ch1_;
+    InputInterface<int> weight_ch2_;
 
     bool log_enable_ = false;
     uint32_t log_counter_ = 0;
@@ -72,5 +78,4 @@ private:
 } // namespace rmcs_core::controller::dart
 
 #include <pluginlib/class_list_macros.hpp>
-PLUGINLIB_EXPORT_CLASS(
-    rmcs_core::controller::dart::DartSettingController, rmcs_executor::Component)
+PLUGINLIB_EXPORT_CLASS(rmcs_core::controller::dart::DartSettingController, rmcs_executor::Component)
--- rmcs_ws/src/rmcs_core/src/hardware/catapult_dart.cpp
+++ rmcs_ws/src/rmcs_core/src/hardware/catapult_dart.cpp
@@ -7,6 +7,7 @@
 #include <iomanip>
 #include <memory>
 #include <mutex>
+#include <rclcpp/subscription.hpp>
 #include <span>
 #include <sstream>
 #include <string>
@@ -52,8 +53,7 @@ class CatapultDartV3Lk
     , private librmcs::agent::CBoard {
 public:
     CatapultDartV3Lk()
-        : Node{get_component_name(),
-               rclcpp::NodeOptions{}.automatically_declare_parameters_from_overrides(true)}
+        : Node{get_component_name(), rclcpp::NodeOptions{}.automatically_declare_parameters_from_overrides(true)}
         , librmcs::agent::CBoard{get_parameter("serial_filter").as_string()}
         , dart_command_(
               create_partner_component<DartCommand>(get_component_name() + "_command", *this))
@@ -91,11 +91,9 @@ public:
                 .set_reduction_ratio(19.));
 
         lifting_left_motor_.configure(
-            device::LkMotor::Config{device::LkMotor::Type::kMG4005Ei10}
-                .enable_multi_turn_angle());
+            device::LkMotor::Config{device::LkMotor::Type::kMG4005Ei10}.enable_multi_turn_angle());
         lifting_right_motor_.configure(
-            device::LkMotor::Config{device::LkMotor::Type::kMG4005Ei10}
-                .enable_multi_turn_angle());
+            device::LkMotor::Config{device::LkMotor::Type::kMG4005Ei10}.enable_multi_turn_angle());
 
         first_sample_spot_ = this->get_parameter("first_sample_spot").as_double();
         final_sample_spot_ = this->get_parameter("final_sample_spot").as_double();
@@ -107,13 +105,18 @@ public:
 
         register_output("/tf", tf_);
         register_output("/imu/catapult_pitch_angle", catapult_pitch_angle_);
-        register_output("/imu/catapult_roll_angle",  catapult_roll_angle_);
-        register_output("/imu/catapult_yaw_angle",   catapult_yaw_angle_);
+        register_output("/imu/catapult_roll_angle", catapult_roll_angle_);
+        register_output("/imu/catapult_yaw_angle", catapult_yaw_angle_);
 
         limiting_calibrate_subscription_ = create_subscription<std_msgs::msg::Int32>(
-            "/limiting/calibrate", rclcpp::QoS{0},
-            [this](std_msgs::msg::Int32::UniquePtr&& msg) {
+            "/limiting/calibrate", rclcpp::QoS{0}, [this](std_msgs::msg::Int32::UniquePtr&& msg) {
                 trigger_servo_calibrate_subscription_callback(limiting_servo_, std::move(msg));
+            });
+
+        force_sensor_calibrate_ = create_subscription<std_msgs::msg::Int32>(
+            "/force_sensor/calibrate", rclcpp::QoS{0},
+            [this](std_msgs::msg::Int32::UniquePtr&& msg) {
+                force_sensor_calibrate_subscription_callback(std::move(msg));
             });
 
         register_output("/referee/serial", referee_serial_);
@@ -128,16 +131,18 @@ public:
         };
         referee_serial_->write = [this](const std::byte* buffer, size_t size) -> size_t {
             auto board = start_transmit();
-            board.uart1_transmit({.uart_data = std::span{buffer, size}});
+            board.uart1_transmit({
+                .uart_data = std::span{buffer, size}
+            });
             return size;
         };
     }
 
-    CatapultDartV3Lk(const CatapultDartV3Lk&)            = delete;
+    CatapultDartV3Lk(const CatapultDartV3Lk&) = delete;
     CatapultDartV3Lk& operator=(const CatapultDartV3Lk&) = delete;
-    CatapultDartV3Lk(CatapultDartV3Lk&&)                 = delete;
-    CatapultDartV3Lk& operator=(CatapultDartV3Lk&&)      = delete;
-    ~CatapultDartV3Lk() override                         = default;
+    CatapultDartV3Lk(CatapultDartV3Lk&&) = delete;
+    CatapultDartV3Lk& operator=(CatapultDartV3Lk&&) = delete;
+    ~CatapultDartV3Lk() override = default;
 
     void update() override {
         dr16_.update_status();
@@ -162,7 +167,7 @@ public:
         // Force sensor: polling command on CAN1 (every 100 cycles)
         if (pub_time_count_++ > 100) {
             board.can1_transmit({
-                .can_id   = 0x301,
+                .can_id = 0x301,
                 .can_data = device::CanPacket8{0}.as_bytes(),
             });
             pub_time_count_ = 0;
@@ -170,43 +175,47 @@ public:
 
         // DJI motors on CAN2 (pitch/yaw/force_screw: 0x200, belts: 0x1FF)
         board.can2_transmit({
-            .can_id   = 0x200,
-            .can_data = device::CanPacket8{
-                            pitch_motor_.generate_command(),
-                            yaw_motor_.generate_command(),
-                            force_screw_motor_.generate_command(),
-                            device::CanPacket8::PaddingQuarter{},
-                        }.as_bytes(),
+            .can_id = 0x200,
+            .can_data =
+                device::CanPacket8{
+                                   pitch_motor_.generate_command(),
+                                   yaw_motor_.generate_command(),
+                                   force_screw_motor_.generate_command(),
+                                   device::CanPacket8::PaddingQuarter{},
+                                   }
+                    .as_bytes(),
         });
         board.can2_transmit({
-            .can_id   = 0x1FF,
-            .can_data = device::CanPacket8{
-                            drive_belt_motor_left_.generate_command(),
-                            drive_belt_motor_right_.generate_command(),
-                            device::CanPacket8::PaddingQuarter{},
-                            device::CanPacket8::PaddingQuarter{},
-                        }.as_bytes(),
+            .can_id = 0x1FF,
+            .can_data =
+                device::CanPacket8{
+                                   drive_belt_motor_left_.generate_command(),
+                                   drive_belt_motor_right_.generate_command(),
+                                   device::CanPacket8::PaddingQuarter{},
+                                   device::CanPacket8::PaddingQuarter{},
+                                   }
+                    .as_bytes(),
         });
 
         // LK4005 lifting motors on CAN1 (unicast per motor)
-        board.can1_transmit({
-            .can_id   = LK_LIFTING_LEFT_ID,
-            .can_data = lifting_left_motor_.generate_velocity_command().as_bytes(),
-        });
-        board.can1_transmit({
-            .can_id   = LK_LIFTING_RIGHT_ID,
-            .can_data = lifting_right_motor_.generate_velocity_command().as_bytes(),
-        });
+        // board.can1_transmit({
+        //     .can_id   = LK_LIFTING_LEFT_ID,
+        //     .can_data = lifting_left_motor_.generate_velocity_command().as_bytes(),
+        // });
+        // board.can1_transmit({
+        //     .can_id   = LK_LIFTING_RIGHT_ID,
+        //     .can_data = lifting_right_motor_.generate_velocity_command().as_bytes(),
+        // });
 
         // Limiting servo on UART2 (only send when target changes)
         if (!limiting_servo_.calibrate_mode()) {
             uint16_t current_target = limiting_servo_.get_target_angle();
             if (current_target != last_limiting_angle_) {
                 size_t uart_data_length;
-                auto command_buffer =
-                    limiting_servo_.generate_runtime_command(uart_data_length);
-                board.uart2_transmit(
-                    {.uart_data = std::span{command_buffer.get(), uart_data_length}});
+                auto command_buffer = limiting_servo_.generate_runtime_command(uart_data_length);
+                board.uart2_transmit({
+                    .uart_data = std::span{command_buffer.get(), uart_data_length}
+                });
                 last_limiting_angle_ = current_target;
             }
         }
@@ -222,8 +231,7 @@ protected:
             force_sensor_frame_count_++;
             if (force_sensor_frame_count_ % 200 == 1) {
                 RCLCPP_INFO(
-                    logger_, "[ForceSensor CAN1:0x302] raw(%zu): %s",
-                    data.can_data.size(),
+                    logger_, "[ForceSensor CAN1:0x302] raw(%zu): %s", data.can_data.size(),
                     bytes_to_hex_string(data.can_data.data(), data.can_data.size()).c_str());
             }
             force_sensor_.store_status(data.can_data);
@@ -239,13 +247,12 @@ protected:
             return;
         const auto can_id = data.can_id;
 
-        if (can_id != 0x201 && can_id != 0x202 && can_id != 0x203
-            && can_id != 0x205 && can_id != 0x206) {
+        if (can_id != 0x201 && can_id != 0x202 && can_id != 0x203 && can_id != 0x205
+            && can_id != 0x206) {
             can2_unknown_count_++;
             if (can2_unknown_count_ % 50 == 1) {
                 RCLCPP_INFO(
-                    logger_, "[CAN2 unknown] id=0x%03X raw(%zu): %s",
-                    can_id, data.can_data.size(),
+                    logger_, "[CAN2 unknown] id=0x%03X raw(%zu): %s", can_id, data.can_data.size(),
                     bytes_to_hex_string(data.can_data.data(), data.can_data.size()).c_str());
             }
         }
@@ -272,10 +279,11 @@ protected:
 
     void uart2_receive_callback(const librmcs::data::UartDataView& data) override {
         const auto* raw_data = data.uart_data.data();
-        const size_t length  = data.uart_data.size();
+        const size_t length = data.uart_data.size();
 
-        RCLCPP_DEBUG(logger_, "UART2 received: len=%zu [%s]", length,
-                     bytes_to_hex_string(raw_data, length).c_str());
+        RCLCPP_DEBUG(
+            logger_, "UART2 received: len=%zu [%s]", length,
+            bytes_to_hex_string(raw_data, length).c_str());
 
         if (length < 3) {
             RCLCPP_WARN(logger_, "UART2 data too short: %zu bytes", length);
@@ -298,8 +306,7 @@ protected:
         dr16_.store_status(data.uart_data.data(), data.uart_data.size());
     }
 
-    void accelerometer_receive_callback(
-        const librmcs::data::AccelerometerDataView& data) override {
+    void accelerometer_receive_callback(const librmcs::data::AccelerometerDataView& data) override {
         imu_.store_accelerometer_status(data.x, data.y, data.z);
     }
 
@@ -308,9 +315,17 @@ protected:
     }
 
 private:
-    static constexpr uint8_t  LIMITING_SERVO_ID = 0x03;
-    static constexpr uint32_t LK_LIFTING_LEFT_ID  = 0x141;
+    static constexpr uint8_t LIMITING_SERVO_ID = 0x03;
+    static constexpr uint32_t LK_LIFTING_LEFT_ID = 0x141;
     static constexpr uint32_t LK_LIFTING_RIGHT_ID = 0x145;
+
+    void force_sensor_calibrate_subscription_callback(std_msgs::msg::Int32::UniquePtr msg) {
+        auto board = start_transmit();
+        board.can1_transmit({
+            .can_id = 0x201,
+            .can_data = device::CanPacket8{0}.as_bytes(),
+        });
+    }
 
     void trigger_servo_calibrate_subscription_callback(
         device::TriggerServo& servo, std_msgs::msg::Int32::UniquePtr msg) {
@@ -344,10 +359,12 @@ private:
 
         if (command_buffer && command_length > 0) {
             auto board = start_transmit();
-            board.uart2_transmit(
-                {.uart_data = std::span{command_buffer.get(), command_length}});
-            RCLCPP_INFO(logger_, "UART2 Pub: (length=%zu)[ %s ]", command_length,
-                        bytes_to_hex_string(command_buffer.get(), command_length).c_str());
+            board.uart2_transmit({
+                .uart_data = std::span{command_buffer.get(), command_length}
+            });
+            RCLCPP_INFO(
+                logger_, "UART2 Pub: (length=%zu)[ %s ]", command_length,
+                bytes_to_hex_string(command_buffer.get(), command_length).c_str());
         }
     }
 
@@ -373,19 +390,18 @@ private:
 
     void processImuData() {
         auto current_time = std::chrono::steady_clock::now();
-        double elapsed_seconds =
-            std::chrono::duration<double>(current_time - start_time_).count();
+        double elapsed_seconds = std::chrono::duration<double>(current_time - start_time_).count();
 
         Eigen::Quaterniond imu_quat(imu_.q0(), imu_.q1(), imu_.q2(), imu_.q3());
         Eigen::Matrix3d rot = imu_quat.toRotationMatrix();
 
-        double roll  = std::atan2(rot(2, 1), rot(2, 2));
+        double roll = std::atan2(rot(2, 1), rot(2, 2));
         double pitch = std::asin(-rot(2, 0));
-        double yaw   = std::atan2(rot(1, 0), rot(0, 0));
+        double yaw = std::atan2(rot(1, 0), rot(0, 0));
 
-        double t_roll  = -roll_filter_.update(roll);
-        double t_pitch =  pitch_filter_.update(pitch);
-        double t_yaw   = -yaw_filter_.update(yaw);
+        double t_roll = -roll_filter_.update(roll);
+        double t_pitch = pitch_filter_.update(pitch);
+        double t_yaw = -yaw_filter_.update(yaw);
 
         if (elapsed_seconds >= first_sample_spot_ && elapsed_seconds <= final_sample_spot_) {
             yaw_samples_.push_back(t_yaw);
@@ -394,8 +410,8 @@ private:
             double sum_x = 0.0, sum_y = 0.0, sum_xy = 0.0, sum_xx = 0.0;
             double n = static_cast<double>(yaw_samples_.size());
             for (size_t i = 0; i < yaw_samples_.size(); ++i) {
-                sum_x  += time_samples_[i];
-                sum_y  += yaw_samples_[i];
+                sum_x += time_samples_[i];
+                sum_y += yaw_samples_[i];
                 sum_xy += time_samples_[i] * yaw_samples_[i];
                 sum_xx += time_samples_[i] * time_samples_[i];
             }
@@ -407,50 +423,53 @@ private:
         t_yaw -= (yaw_drift_coefficient_ + 0.000512) * elapsed_seconds;
         publishTfTransforms(t_roll, t_pitch, t_yaw);
 
-        *catapult_roll_angle_  = (t_roll  / M_PI) * 180.0;
+        *catapult_roll_angle_ = (t_roll / M_PI) * 180.0;
         *catapult_pitch_angle_ = (t_pitch / M_PI) * 180.0;
-        *catapult_yaw_angle_   = (t_yaw   / M_PI) * 180.0;
+        *catapult_yaw_angle_ = (t_yaw / M_PI) * 180.0;
     }
 
     void publishTfTransforms(double roll, double pitch, double yaw) {
         auto now = this->get_clock()->now();
 
-        auto make_tf =
-            [&](const std::string& parent, const std::string& child,
-                const geometry_msgs::msg::Vector3& trans,
-                const geometry_msgs::msg::Quaternion& rot) {
-                geometry_msgs::msg::TransformStamped tf;
-                tf.header.stamp    = now;
-                tf.header.frame_id = parent;
-                tf.child_frame_id  = child;
-                tf.transform.translation = trans;
-                tf.transform.rotation    = rot;
-                return tf;
-            };
+        auto make_tf = [&](const std::string& parent, const std::string& child,
+                           const geometry_msgs::msg::Vector3& trans,
+                           const geometry_msgs::msg::Quaternion& rot) {
+            geometry_msgs::msg::TransformStamped tf;
+            tf.header.stamp = now;
+            tf.header.frame_id = parent;
+            tf.child_frame_id = child;
+            tf.transform.translation = trans;
+            tf.transform.rotation = rot;
+            return tf;
+        };
 
         auto make_vec = [](double x, double y, double z) {
             geometry_msgs::msg::Vector3 v;
-            v.x = x; v.y = y; v.z = z;
+            v.x = x;
+            v.y = y;
+            v.z = z;
             return v;
         };
 
         auto make_quat = [](double x, double y, double z, double w) {
             geometry_msgs::msg::Quaternion q;
-            q.x = x; q.y = y; q.z = z; q.w = w;
+            q.x = x;
+            q.y = y;
+            q.z = z;
+            q.w = w;
             return q;
         };
 
-        Eigen::Quaterniond roll_q(Eigen::AngleAxisd(roll,  Eigen::Vector3d::UnitX()));
+        Eigen::Quaterniond roll_q(Eigen::AngleAxisd(roll, Eigen::Vector3d::UnitX()));
         Eigen::Quaterniond pitch_q(Eigen::AngleAxisd(pitch, Eigen::Vector3d::UnitY()));
-        Eigen::Quaterniond yaw_q(Eigen::AngleAxisd(yaw,   Eigen::Vector3d::UnitZ()));
+        Eigen::Quaterniond yaw_q(Eigen::AngleAxisd(yaw, Eigen::Vector3d::UnitZ()));
         Eigen::Quaterniond combined = yaw_q * pitch_q * roll_q;
 
-        auto zero    = make_vec(0, 0, 0);
+        auto zero = make_vec(0, 0, 0);
         auto p_trans = make_vec(0, 0, 0.05);
-        auto id_rot  = make_quat(0, 0, 0, 1);
+        auto id_rot = make_quat(0, 0, 0, 1);
 
-        tf_broadcaster_->sendTransform(
-            make_tf("base_link", "gimbal_center_link", zero, id_rot));
+        tf_broadcaster_->sendTransform(make_tf("base_link", "gimbal_center_link", zero, id_rot));
         tf_broadcaster_->sendTransform(make_tf(
             "gimbal_center_link", "yaw_link", zero,
             make_quat(yaw_q.x(), yaw_q.y(), yaw_q.z(), yaw_q.w())));
@@ -460,8 +479,7 @@ private:
         tf_broadcaster_->sendTransform(make_tf(
             "pitch_link", "odom_imu", zero,
             make_quat(combined.x(), combined.y(), combined.z(), combined.w())));
-        tf_broadcaster_->sendTransform(
-            make_tf("world", "base_link", zero, id_rot));
+        tf_broadcaster_->sendTransform(make_tf("world", "base_link", zero, id_rot));
     }
 
     class DartCommand : public rmcs_executor::Component {
@@ -494,14 +512,14 @@ private:
 
     device::ForceSensor force_sensor_;
 
-    device::PWMServo    trigger_servo_;
+    device::PWMServo trigger_servo_;
     device::TriggerServo limiting_servo_;
 
     // LK4005 lifting motors (replaces TriggerServo lifting servos)
     device::LkMotor lifting_left_motor_;
     device::LkMotor lifting_right_motor_;
 
-    device::Dr16  dr16_;
+    device::Dr16 dr16_;
     device::Bmi088 imu_;
 
     std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
@@ -512,17 +530,18 @@ private:
     OutputInterface<double> catapult_yaw_angle_;
 
     rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr limiting_calibrate_subscription_;
+    rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr force_sensor_calibrate_;
 
     std::mutex referee_mutex_;
     std::deque<std::byte> referee_ring_buffer_receive_;
     OutputInterface<rmcs_msgs::SerialInterface> referee_serial_;
 
-    int pub_time_count_         = 0;
+    int pub_time_count_ = 0;
     int force_sensor_frame_count_ = 0;
-    int can2_unknown_count_     = 0;
+    int can2_unknown_count_ = 0;
 
-    double first_sample_spot_     = 1.0;
-    double final_sample_spot_     = 4.0;
+    double first_sample_spot_ = 1.0;
+    double final_sample_spot_ = 4.0;
     double yaw_drift_coefficient_ = 0.0;
     std::vector<double> yaw_samples_;
     std::vector<double> time_samples_;
