d15b52c011c160ecf192a5319e694f3e8a2c8e06
zgz713912 <zgz713912@gmail.com>
2026-03-31 21:43:51 +0800

    rename Smc_Observer to SmcObserver and remove unused parameters

--- rmcs_ws/src/rmcs_core/src/controller/smc/smc_observer.cpp
+++ rmcs_ws/src/rmcs_core/src/controller/smc/smc_observer.cpp
@@ -7,32 +7,27 @@
 #include <rmcs_executor/component.hpp>
 
 namespace rmcs_core::controller::smc {
-class Smc_Observer
+class SmcObserver
     : public rmcs_executor::Component
     , public rclcpp::Node {
 public:
-    Smc_Observer()
+    SmcObserver()
         : Node(get_component_name(), rclcpp::NodeOptions{}.automatically_declare_parameters_from_overrides(true))
         , logger_(get_logger()) {
         // gear_ratio_         = get_parameter("gear_ratio").as_double();
-        velocity_limit_     = get_parameter("velocity_limit").as_double();
-        acceleration_limit_ = get_parameter("acceleration_limit").as_double();
+        velocity_limit_ = get_parameter("velocity_limit").as_double();
 
         register_input(get_parameter("control_error").as_string(), yaw_angle_error_);
         register_input(get_parameter("velocity").as_string(), yaw_velocity_);
 
         register_output(get_parameter("yaw_expect_velocity_").as_string(), yaw_expect_velocity_);
-        register_output(get_parameter("yaw_expect_acceleration_").as_string(), yaw_expect_acceleration_);
     }
 
     void update() override {
-        // update_gimbal_status();
         update_expect_value();
     }
 
 private:
-    // void update_gimbal_status() { *yaw_velocity_ = *yaw_motor_velocity_ * gear_ratio_; }
-
     void update_expect_value() {
         yaw_angle_error_window_.push_back(-*yaw_angle_error_);
         if (yaw_angle_error_window_.size() > max_window_size_) {
@@ -42,14 +37,6 @@ private:
         double expect_velocity_error = least_square_calculator(yaw_angle_error_window_) / acquisition_interval;
 
         *yaw_expect_velocity_ = std::clamp(*yaw_velocity_ - expect_velocity_error, -velocity_limit_, velocity_limit_);
-
-        yaw_expect_velocity_window_.push_back(*yaw_expect_velocity_);
-        if (yaw_expect_velocity_window_.size() > max_window_size_) {
-            yaw_expect_velocity_window_.pop_front();
-        }
-        double expect_acceleration = least_square_calculator(yaw_expect_velocity_window_) / acquisition_interval;
-
-        *yaw_expect_acceleration_ = std::clamp(expect_acceleration, -acceleration_limit_, acceleration_limit_);
     }
 
     static double least_square_calculator(const std::deque<double>& value_window) {
@@ -92,20 +79,17 @@ private:
     const double acquisition_interval = 0.001; // s
 
     double velocity_limit_;
-    double acceleration_limit_;
 
     InputInterface<double> yaw_angle_error_;
     InputInterface<double> yaw_velocity_;
 
     std::deque<double> yaw_angle_error_window_;
-    std::deque<double> yaw_expect_velocity_window_;
 
     // OutputInterface<double> yaw_velocity_;
     OutputInterface<double> yaw_expect_velocity_;
-    OutputInterface<double> yaw_expect_acceleration_;
 };
 } // namespace rmcs_core::controller::smc
 
 #include <pluginlib/class_list_macros.hpp>
 
-PLUGINLIB_EXPORT_CLASS(rmcs_core::controller::smc::Smc_Observer, rmcs_executor::Component)
+PLUGINLIB_EXPORT_CLASS(rmcs_core::controller::smc::SmcObserver, rmcs_executor::Component)
