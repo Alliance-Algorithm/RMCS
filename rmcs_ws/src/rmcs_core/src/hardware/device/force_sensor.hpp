4769e96bddd6a86027ef1297cae364a86cf03524
floatpigeon <floatpigeon@proton.me>
2026-04-11 18:41:40 +0800

    Unifiled force sensor data type

--- rmcs_ws/src/rmcs_core/src/hardware/device/force_sensor.hpp
+++ rmcs_ws/src/rmcs_core/src/hardware/device/force_sensor.hpp
@@ -6,7 +6,6 @@
 #include <cmath>
 #include <cstdint>
 #include <cstring>
-#include <limits>
 #include <rmcs_executor/component.hpp>
 
 namespace rmcs_core::hardware::device {
@@ -14,14 +13,13 @@ namespace rmcs_core::hardware::device {
 class ForceSensor {
 public:
     explicit ForceSensor(rmcs_executor::Component& status_component) {
-        status_component.register_output("/force_sensor/channel_1/weight", weight_ch1_, nan_);
-        status_component.register_output("/force_sensor/channel_2/weight", weight_ch2_, nan_);
+        status_component.register_output("/force_sensor/channel_1/weight", weight_ch1_, int32_t{0});
+        status_component.register_output("/force_sensor/channel_2/weight", weight_ch2_, int32_t{0});
     }
 
     void store_status(std::span<const std::byte> can_data) {
         ForceSensorStatus status{};
-        std::memcpy(&status, can_data.data(),
-                    std::min(can_data.size(), sizeof(ForceSensorStatus)));
+        std::memcpy(&status, can_data.data(), std::min(can_data.size(), sizeof(ForceSensorStatus)));
         force_sensor_status_.store(status, std::memory_order_relaxed);
     }
 
@@ -30,24 +28,21 @@ public:
 
         uint32_t ch1_weight =
             static_cast<uint32_t>(status.ch1_0) << 24 | static_cast<uint32_t>(status.ch1_1) << 16
-            | static_cast<uint32_t>(status.ch1_2) << 8  | static_cast<uint32_t>(status.ch1_3);
+            | static_cast<uint32_t>(status.ch1_2) << 8 | static_cast<uint32_t>(status.ch1_3);
         uint32_t ch2_weight =
             static_cast<uint32_t>(status.ch2_0) << 24 | static_cast<uint32_t>(status.ch2_1) << 16
-            | static_cast<uint32_t>(status.ch2_2) << 8  | static_cast<uint32_t>(status.ch2_3);
+            | static_cast<uint32_t>(status.ch2_2) << 8 | static_cast<uint32_t>(status.ch2_3);
 
-        *weight_ch1_ = std::bit_cast<int>(ch1_weight);
-        *weight_ch2_ = std::bit_cast<int>(ch2_weight);
+        *weight_ch1_ = std::bit_cast<int32_t>(ch1_weight);
+        *weight_ch2_ = std::bit_cast<int32_t>(ch2_weight);
     }
 
     // static uint64_t generate_command() { return 0x00; }
     static uint64_t generate_zero_calibration_command() { return 0x0F; }
 
-
-    static CanPacket8::Quarter generate_command(){return CanPacket8::Quarter{0};}
+    static CanPacket8::Quarter generate_command() { return CanPacket8::Quarter{0}; }
 
 private:
-    static constexpr double nan_ = std::numeric_limits<int>::quiet_NaN();
-
     struct __attribute__((packed, aligned(8))) ForceSensorStatus {
         uint8_t ch1_0, ch1_1, ch1_2, ch1_3;
         uint8_t ch2_0, ch2_1, ch2_2, ch2_3;
@@ -55,8 +50,8 @@ private:
     std::atomic<ForceSensorStatus> force_sensor_status_{};
     static_assert(decltype(force_sensor_status_)::is_always_lock_free);
 
-    rmcs_executor::Component::OutputInterface<int> weight_ch1_;
-    rmcs_executor::Component::OutputInterface<int> weight_ch2_;
+    rmcs_executor::Component::OutputInterface<int32_t> weight_ch1_;
+    rmcs_executor::Component::OutputInterface<int32_t> weight_ch2_;
 };
 
 } // namespace rmcs_core::hardware::device
