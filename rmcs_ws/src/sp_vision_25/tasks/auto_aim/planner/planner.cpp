6501c36095c6dcd437e487572d1dcc620fd4a0ff
dwx <1591215786@qq.com>
2026-04-24 16:45:06 +0800

    mouse

--- rmcs_ws/src/sp_vision_25/tasks/auto_aim/planner/planner.cpp
+++ rmcs_ws/src/sp_vision_25/tasks/auto_aim/planner/planner.cpp
@@ -435,7 +435,7 @@ Plan Planner::plan_from_shot_state(Target target_at_shot, double bullet_speed) {
     clear_debug_targets();
 
     if (bullet_speed < 10 || bullet_speed > 12)
-        bullet_speed = 11.5;
+        bullet_speed = 11.4;
 
     const TrackingMode mode = select_tracking_mode(target_at_shot);
 
