a0cbb14c267ccd9a5443e759ea378136aa2c8a6b
heyeuu <2829004293@qq.com>
2026-07-03 13:33:25 +0800

    feat: extract FlightGimbalController from SimpleGimbalController
    - Revert simple_gimbal_controller to align with main branch
    - Create flight_gimbal_controller with flight-specific logic
      (auto_aim/should_control, allFinite() check, adjusted mouse pitch sign)
    - Register FlightGimbalController in plugins.xml
    - Update flight.yaml to use FlightGimbalController

--- rmcs_ws/src/rmcs_core/src/controller/gimbal/flight_gimbal_controller.cpp
+++ rmcs_ws/src/rmcs_core/src/controller/gimbal/flight_gimbal_controller.cpp
@@ -12,11 +12,11 @@ namespace rmcs_core::controller::gimbal {
 
 using namespace rmcs_description;
 
-class SimpleGimbalController
+class FlightGimbalController
     : public rmcs_executor::Component
     , public rclcpp::Node {
 public:
-    SimpleGimbalController()
+    FlightGimbalController()
         : Node(
               get_component_name(),
               rclcpp::NodeOptions{}.automatically_declare_parameters_from_overrides(true))
@@ -30,7 +30,8 @@ public:
         register_input("/remote/mouse/velocity", mouse_velocity_);
         register_input("/remote/mouse", mouse_);
 
-        register_input("/gimbal/auto_aim/control_direction", auto_aim_control_direction_, false);
+        register_input("/auto_aim/should_control", auto_aim_should_control_, false);
+        register_input("/auto_aim/control_direction", auto_aim_control_direction_, false);
 
         register_output("/gimbal/yaw/control_angle_error", yaw_angle_error_, nan_);
         register_output("/gimbal/pitch/control_angle_error", pitch_angle_error_, nan_);
@@ -45,18 +46,22 @@ public:
     TwoAxisGimbalSolver::AngleError calculate_angle_error() {
         auto switch_right = *switch_right_;
         auto switch_left = *switch_left_;
-        auto mouse = *mouse_;
 
         using namespace rmcs_msgs;
         if ((switch_left == Switch::UNKNOWN || switch_right == Switch::UNKNOWN)
             || (switch_left == Switch::DOWN && switch_right == Switch::DOWN))
             return two_axis_gimbal_solver.update(TwoAxisGimbalSolver::SetDisabled());
 
-        if (auto_aim_control_direction_.ready() && (mouse.right || switch_right == Switch::UP)
-            && !auto_aim_control_direction_->isZero())
+        const auto auto_aim_requested = mouse_->right || switch_right == Switch::UP;
+        const auto should_control = auto_aim_should_control_.ready() && *auto_aim_should_control_;
+        const auto valid_control =
+            auto_aim_control_direction_.ready() && auto_aim_control_direction_->allFinite();
+
+        if (auto_aim_requested && should_control && valid_control) {
             return two_axis_gimbal_solver.update(
                 TwoAxisGimbalSolver::SetControlDirection(
                     OdomImu::DirectionVector(*auto_aim_control_direction_)));
+        }
 
         if (!two_axis_gimbal_solver.enabled())
             return two_axis_gimbal_solver.update(TwoAxisGimbalSolver::SetToLevel());
@@ -67,7 +72,7 @@ public:
         double yaw_shift =
             joystick_sensitivity * joystick_left_->y() + mouse_sensitivity * mouse_velocity_->y();
         double pitch_shift =
-            -joystick_sensitivity * joystick_left_->x() - mouse_sensitivity * mouse_velocity_->x();
+            -joystick_sensitivity * joystick_left_->x() + mouse_sensitivity * mouse_velocity_->x();
 
         return two_axis_gimbal_solver.update(
             TwoAxisGimbalSolver::SetControlShift(yaw_shift, pitch_shift));
@@ -82,6 +87,7 @@ private:
     InputInterface<Eigen::Vector2d> mouse_velocity_;
     InputInterface<rmcs_msgs::Mouse> mouse_;
 
+    InputInterface<bool> auto_aim_should_control_;
     InputInterface<Eigen::Vector3d> auto_aim_control_direction_;
 
     TwoAxisGimbalSolver two_axis_gimbal_solver;
@@ -94,4 +100,4 @@ private:
 #include <pluginlib/class_list_macros.hpp>
 
 PLUGINLIB_EXPORT_CLASS(
-    rmcs_core::controller::gimbal::SimpleGimbalController, rmcs_executor::Component)
+    rmcs_core::controller::gimbal::FlightGimbalController, rmcs_executor::Component)
