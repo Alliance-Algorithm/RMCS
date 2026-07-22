1efdf815fcefcf480efa613f27e4d194203316f7
zlq040222 <1542498005@qq.com>
2026-07-23 00:18:25 +0800

    feat(msgs): add CLIMB/SUPPORT_ARM modes and need_power

--- rmcs_ws/src/rmcs_msgs/include/rmcs_msgs/chassis_mode.hpp
+++ rmcs_ws/src/rmcs_msgs/include/rmcs_msgs/chassis_mode.hpp
@@ -12,11 +12,19 @@ enum class ChassisMode : uint8_t {
     LAUNCH_RAMP,
     ALIGNMENT,
     ALIGNMENT_POWERED,
+    CLIMB,
+    SUPPORT_ARM,
 };
 
 constexpr auto is_powered(ChassisMode mode) noexcept {
     return mode == ChassisMode::ALIGNMENT_POWERED || mode == ChassisMode::LAUNCH_RAMP;
 }
+
+constexpr auto need_power(ChassisMode mode) noexcept {
+    return mode == ChassisMode::ALIGNMENT_POWERED || mode == ChassisMode::LAUNCH_RAMP
+        || mode == ChassisMode::CLIMB || mode == ChassisMode::SUPPORT_ARM;
+}
+
 constexpr auto is_spining(ChassisMode mode) noexcept {
     return mode == ChassisMode::SPIN_SLOW || mode == ChassisMode::SPIN_FAST;
 }
