8cd3c12621283a43fc4f3f7c5be423bba8e5ca1f
zlq040222 <1542498005@qq.com>
2026-08-06 04:19:45 +0800

    feat:poweredattack posture heatcontroller

--- rmcs_ws/src/rmcs_core/src/controller/shooting/heat_controller.cpp
+++ rmcs_ws/src/rmcs_core/src/controller/shooting/heat_controller.cpp
@@ -18,6 +18,7 @@ public:
 
         register_input("/referee/shooter/cooling", shooter_cooling_);
         register_input("/referee/shooter/heat_limit", shooter_heat_limit_);
+        register_input("/referee/sentry/posture", sentry_posture_, uint8_t{3});
 
         register_input("/gimbal/bullet_fired", bullet_fired_);
 
@@ -28,6 +29,11 @@ public:
     void update() override {
         shooter_heat_ = std::max<int64_t>(0, shooter_heat_ - *shooter_cooling_);
 
+        if (*sentry_posture_ == kPoweredAttackPosture) {
+            *control_bullet_allowance_ = kUnlimitedBulletAllowance;
+            return;
+        }
+
         if (*bullet_fired_)
             shooter_heat_ += heat_per_shot + 10;
 
@@ -36,8 +42,13 @@ public:
     }
 
 private:
+    static constexpr int64_t kPoweredAttackPosture = 4;
+    static constexpr int64_t kUnlimitedBulletAllowance = 100000;
+
     InputInterface<int64_t> shooter_cooling_;
     InputInterface<int64_t> shooter_heat_limit_;
+
+    InputInterface<uint8_t> sentry_posture_;
 
     InputInterface<bool> bullet_fired_;
 
